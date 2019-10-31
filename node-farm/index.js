617123a7e1b53b810c446342e49468d652371dca
Nikola Obradovic <obradovicnik01@gmail.com>
2019-10-31 15:12:07 +0100

    blocking vs non-blocking :)

--- node-farm/index.js
+++ node-farm/index.js
@@ -1,12 +1,24 @@
 const fs = require("fs");
 
-const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
+// Blocking , synchronous way
+// const textIn = fs.readFileSync("./txt/input.txt", "utf-8");
+// // console.log(textIn);
+// const textOut = `somebody once told me the world is gonna roll me ${textIn}.\nCreated on ${Date.now()}`;
+// fs.writeFileSync("./txt/output.txt", textOut);
+// console.log("file has been written");
+// console.log(textOut);
 
-// console.log(textIn);
+// Non-Blocking , synchronous way
+fs.readFile("./txt/start.txt", "utf-8", (err, data1) => {
+  if (err) return console.log("object");
+  fs.readFile(`./txt/${data1}.txt`, "utf-8", (err, data2) => {
+    console.log(data2);
+    fs.readFile(`./txt/append.txt`, "utf-8", (err, data3) => {
+      console.log(data3);
 
-const textOut = `somebody once told me the world is gonna roll me ${textIn}.\nCreated on ${Date.now()}`;
-
-fs.writeFileSync("./txt/output.txt", textOut);
-console.log("file has been written");
-
-console.log(textOut);
+      fs.writeFile("./txt/final.txt", `${data2}\n${data3}`, "utf-8", err => {
+        console.log("File has been written");
+      });
+    });
+  });
+});
